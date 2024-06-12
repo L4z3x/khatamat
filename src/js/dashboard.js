111afbbc1314cf7e59653a4dfc8fa0301f1f859e
L4z3x <moussaousselmal1970@gmail.com>
2024-06-12 01:06:55 +0100

    added functionality to dashboard.

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,16 +1,19 @@
-import { useEffect ,useState,useRef } from 'react'
+import React , { useEffect ,useState,useRef, createElement } from 'react';
 import '../style/dashboard.css'
 
 function DashBoard(){
-    const [time, setTime] = useState({
+   
+    const [part , setPart] = useState([])  // <--- for storing part that are clicked.
+    const partRefs = useRef([])
+    const [time,setTime]= useState({
         sec: 8,
-        min: 0,
+        min:0,
         hour: 0,
         day: 0
-    });
-    const [part , setPart] = useState([''])  // <--- for storing part that are clicked.
-    const divRef = useRef(null)
-
+    })
+    const [info,setInfo] = useState([' من سورة البقرة  1 الى 35'])   // TODO: add all infos about each part.
+    const [inf,setInf] = useState('')
+    //const [inf_style,setInf_style] = useState({})
     useEffect(()=>{setTimeout(()=>{   // <--- timer function:
         setTime(prevTime => {
             let newTime = { ...prevTime };
@@ -31,7 +34,7 @@ function DashBoard(){
                 }
             }
             return newTime;
-        });},1000)})
+        });},1000)});
                                     // <--- setting the time variable:
         let clock_day  = time.day > 9 ? ` ${time.day}`  : ` 0${time.day}` 
         let clock_hour = time.hour > 9 ? ` ${time.hour}` : ` 0${time.hour}` 
@@ -39,33 +42,44 @@ function DashBoard(){
         let clock_sec = time.sec > 9 ? ` ${time.sec}`: ` 0${time.sec}` 
         let clock = clock_day + ` day` + clock_hour + ` Hour`+ clock_min +` min`+ clock_sec+` sec`
     
-    const handleImgClick = (e) => {  // display info(dropdown maybe?).
-        
-    }
-    const handleSpanClick =(e)=>{       // <--- setting the onclick fun for the child (span).
-        divRef.current.click();
+    
+    const handleSpanClick =(e,i)=>{       // <--- setting the onclick fun for the child (span).
+        partRefs.current[i].click();
         e.stopPropagation();
     }
                                     
     const handlePartOnclick = (e)=>{        // <--- setting the onclick function.
-        console.log(e.target)
         if (e.target.className === 'part') {
-            if (e.target.style.opacity === '' ) {
-                console.log('change opacity to 0.45')
-                e.target.style.opacity = 0.45 ;        // <--- change opacity on click.
+            const newPart = e.target.childNodes[0].innerText; 
+            if (e.target.style.opacity === '' || !part.includes(newPart)) {
+                setPart(value =>([...value,newPart]));
+                e.target.style.opacity = 0.45 ;        // <--- change opacity on click and add part.
             }else{
-                console.log('change style null')
                 e.target.style.opacity ='';
+                setPart(value=>(value.filter(value => value !== newPart)))
             }
         }
-        const newPart = e.target.childNodes[0].innerText;       //TODO: integrate the two functions 
-        if (!part.includes(newPart)) {                          //      (opacity and adding parts).
-            setPart(value =>([...value,newPart]));        // <--- add parts.
-            console.log(part);
-        }else{console.log('already added')}
-        
         e.stopPropagation();   
     }
+    useEffect(()=>{
+        console.log(part);
+    },[part]);
+
+    const showInfoOnHover = (e,i)=>{               // <--- on click (info icon) : add the info.
+        const info = partRefs.current[i].lastChild;
+       console.log(i+1);
+        if ( info.style.display === '') {
+            info.style.display = 'flex';          //TODO: when the info bar is overflowing  
+        }                                         //        adjust the position .  (very hard man bfffff)
+    }
+
+    const handleMouseLeave = (e,i)=>{       // on mouse leave (info icon) : remove the info.
+        const curPart = partRefs.current[i];
+        if (curPart.lastChild.style.display === 'flex') {
+            curPart.lastChild.style.display = null ;
+        }
+    }
+
         return(
         <div className='dash-backround'>
             <div className='timer'>
@@ -75,66 +89,17 @@ function DashBoard(){
                 <h2>pick parts from the quran and read it before the time ends</h2>
             </div>
             <div className='part-container'>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 1</span><img onClick={handleImgClick}className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 2</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 3</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 4</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 5</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 6</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 7</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 8</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 9</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 10</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 11</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 12</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 13</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 14</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 15</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 16</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 17</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 18</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 19</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 20</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 21</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 22</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 23</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 24</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 25</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 26</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 27</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 28</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 29</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 30</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 31</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 32</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 33</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 34</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 35</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 36</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 37</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 38</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 39</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 40</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 41</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 42</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 43</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 44</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef} className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 45</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef}className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 46</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef}className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 47</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef}className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 48</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef}className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 49</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef}className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 50</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef}className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 51</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef}className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 52</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef}className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 53</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef}className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 54</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef}className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 55</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef}className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 56</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef}className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 57</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef}className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 58</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef}className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 59</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
-                <div ref={divRef}className='part' onClick={handlePartOnclick}><span onClick={handleSpanClick}>Part 60</span><img className='info-icon' src={require('../img/info.png')} alt='info-iccon'/></div>
+                {Array.from({ length: 60 }, (_, i) => (                                 // <--- this part is from chatgpt 
+                    <div ref={el => partRefs.current[i] = el} key={i} className='part'  //    shhhh don't tell anyone okey ...
+                    onClick={(e) => handlePartOnclick(e)}>
+                        <div className='part-default'>
+                            <span className='span-part' onClick={(e) => handleSpanClick(e, i)}>Part {i + 1}</span>
+                            <img onMouseOver={(e)=>showInfoOnHover(e,i)} onMouseLeave={(e)=>handleMouseLeave(e,i)}className='info-icon' src={require('../img/info.png')} 
+                            alt='info-icon' />
+                        </div> 
+                        <div className='info-div'><span className='info-span'>gdadadsada</span></div> 
+                    </div>                              // TODO: replace the text with the info variable.
+                ))}
             </div>
         </div>
     )
